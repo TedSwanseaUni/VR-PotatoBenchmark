ea272d1e786f9d51d303baed9cbb8f72b23e0cf7
TedSwanseaUni <e.l.thomas@Swansea.ac.uk>
2023-06-28 16:50:37 +0100

    2023.1 complete

--- Assets/Scripts/ConfigureShadowCascades.cs
+++ Assets/Scripts/ConfigureShadowCascades.cs
@@ -11,7 +11,7 @@ public class ConfigureShadowCascades : MonoBehaviour
     {
         UnityEngine.Rendering.Universal.UniversalRenderPipelineAsset asset = GraphicsSettings.renderPipelineAsset as UnityEngine.Rendering.Universal.UniversalRenderPipelineAsset;
         if (asset != null)
-            asset.shadowCascadeOption = (m_EnableCascades) ? UnityEngine.Rendering.Universal.ShadowCascadesOption.FourCascades : UnityEngine.Rendering.Universal.ShadowCascadesOption.NoCascades;
+            asset.shadowCascadeCount = (m_EnableCascades) ? 4 :0;
         QualitySettings.shadowCascades = (m_EnableCascades) ? 4 : 1;
     }
 }
